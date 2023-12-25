3c5c04df193f2519a9d1a69a86fa8b9e4d012439
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2023-12-26 01:45:43 +0530

    formatted

--- src/app/components/availability-card/availability-card.component.ts
+++ src/app/components/availability-card/availability-card.component.ts
@@ -3,30 +3,34 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-availability-card',
   templateUrl: './availability-card.component.html',
-  styleUrls: ['./availability-card.component.scss']
+  styleUrls: ['./availability-card.component.scss'],
 })
 export class AvailabilityCardComponent {
-
   @Input() avail: any;
 
-  constructor() { }
+  constructor() {}
 
-  // Add these methods in your component class
-getQuotaCardClass(status: string): { [key: string]: boolean } {
-  return {
-    'border-success': status === 'Available' || status === 'CURR_AVBL' || status === 'RAC',
-    'border-warning': status?.indexOf('WL') > 0,
-    'border-danger': status === 'REGRET' || status === 'NOT AVAILABLE' || status === 'CLASS NOT EXIST',
-  };
-}
+  getQuotaCardClass(status: string): { [key: string]: boolean } {
+    return {
+      'border-success':
+        status === 'Available' || status === 'CURR_AVBL' || status === 'RAC',
+      'border-warning': status?.indexOf('WL') > 0,
+      'border-danger':
+        status === 'REGRET' ||
+        status === 'NOT AVAILABLE' ||
+        status === 'CLASS NOT EXIST',
+    };
+  }
 
-getAvailabilityClass(status: string): { [key: string]: boolean } {
-  return {
-    'text-success': status === 'Available' || status === 'CURR_AVBL' || status === 'RAC',
-    'text-warning': status?.indexOf('WL') > 0,
-    'text-danger': status === 'REGRET' || status === 'NOT AVAILABLE' || status === 'CLASS NOT EXIST',
-  };
-}
-
-
+  getAvailabilityClass(status: string): { [key: string]: boolean } {
+    return {
+      'text-success':
+        status === 'Available' || status === 'CURR_AVBL' || status === 'RAC',
+      'text-warning': status?.indexOf('WL') > 0,
+      'text-danger':
+        status === 'REGRET' ||
+        status === 'NOT AVAILABLE' ||
+        status === 'CLASS NOT EXIST',
+    };
+  }
 }

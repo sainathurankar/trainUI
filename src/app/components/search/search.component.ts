9ac5dd24da756dfb110dfb7514774e0b96a24a41
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2023-12-25 17:15:25 +0530

    disabling input suggestions and setting length of autocomplete text call

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -25,7 +25,7 @@ export class SearchComponent implements OnInit {
 
   onInput(key: string): void {
     const input = key === 'from' ? this.frominputValue : this.toinputValue;
-    if(input.length > 0) {
+    if(input.length > 2) {
       // this.suggestions = this.autocompleteService.getSuggestions()
       // .filter(suggestion => suggestion.toLowerCase().includes(this.inputValue.toLowerCase()));
       this.autocompleteService.getSuggestions(input.trim()).subscribe((data) => {

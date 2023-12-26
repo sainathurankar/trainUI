90b21135b8322d6e5bf83e2896cc2dd25e0ed55b
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2023-12-26 12:45:58 +0530

    moving api url to env

--- src/app/services/autocomplete.service.ts
+++ src/app/services/autocomplete.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,6 @@ export class AutocompleteService {
   constructor(private http: HttpClient) { }
 
   getSuggestions(query: string): Observable<any> {
-    return this.http.get('http://localhost:8080/api/autocomplete?query=' + query);
+    return this.http.get(`${environment.apiUrl}/autocomplete?query=${query}`);
   }
 }

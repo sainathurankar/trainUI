6206d7780aa1a237d089bf4be7b91441d8f176b4
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2023-12-28 00:12:19 +0530

    converted To 12 Hour Format

--- src/app/components/train-card/train-card.component.ts
+++ src/app/components/train-card/train-card.component.ts
@@ -1,4 +1,6 @@
+import { DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { Helper } from 'src/app/common/helper';
 
 @Component({
   selector: 'app-train-card',
@@ -7,6 +9,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class TrainCardComponent {
 
+  helper = Helper;
+  
   @Input() train: any;
 
   @Input() doj?: string;

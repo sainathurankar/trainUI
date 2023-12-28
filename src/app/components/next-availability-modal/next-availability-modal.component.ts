eb64a7e3210ab5577f4000b2e94c53ee7042736d
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2023-12-28 15:43:58 +0530

    next 6 days tab to choose class

--- src/app/components/next-availability-modal/next-availability-modal.component.ts
+++ src/app/components/next-availability-modal/next-availability-modal.component.ts
@@ -7,40 +7,71 @@ import { SearchService } from 'src/app/services/search/search.service';
 @Component({
   selector: 'app-next-availability-modal',
   templateUrl: './next-availability-modal.component.html',
-  styleUrls: ['./next-availability-modal.component.scss']
+  styleUrls: ['./next-availability-modal.component.scss'],
 })
-export class NextAvailabilityModalComponent implements OnInit{
-
+export class NextAvailabilityModalComponent implements OnInit {
   @Input() train: any; // Assuming you have a Train interface or class
   @Input() doj: any;
 
-  nextAvailList: any;
-  loading = true;
+  availList: any[] = [];
+  loading = false;
+  loadingNext = false;
 
   helper = Helper;
 
-  constructor(public activeModal: NgbActiveModal, private searchService: SearchService) { }
+  selectedClass: string = '';
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private searchService: SearchService
+  ) {}
   ngOnInit(): void {
-    this.getNextAvailability();
+    this.updateSelectedClass(this.train.availableClasses[0]);
   }
 
-  getNextAvailability() {
-    const trainUpdateInput: TrainUpdateInput = {
+  updateSelectedClass(cls: string) {
+    this.selectedClass = cls;
+    this.availList = [];
+    this.getAvailability();
+  }
+
+  getAvailability() {
+    const trainUpdateInput: TrainUpdateInput = this.buildInput();
+    this.loading = true;
+    this.searchService
+      .getNextAvailability(trainUpdateInput)
+      .subscribe((resonse) => {
+        this.availList = resonse;
+        this.loading = false;
+      });
+  }
+
+  closeModal() {
+    this.activeModal.dismiss('Cross click');
+  }
+
+  loadNextAvail() {
+    this.doj = this.helper.nextDayDate(
+      this.availList[this.availList.length - 1].availablityDate
+    );
+    this.loading = true;
+    this.searchService
+      .getNextAvailability(this.buildInput())
+      .subscribe((response) => {
+        this.availList.push(...response);
+        this.loading = false;
+      });
+  }
+
+  buildInput(): TrainUpdateInput {
+    return {
       source: this.train.fromStationCode,
       destination: this.train.toStationCode,
       doj: this.doj,
       quota: 'GN',
       trainNumber: this.train.trainNumber,
-      class: this.train.availableClasses[0],
+      class: this.selectedClass,
       // numberOfDays: 2
     };
-    this.searchService.getNextAvailability(trainUpdateInput).subscribe((resonse) => {
-      this.nextAvailList = resonse;
-      this.loading = false;
-    })
-  }
-
-  closeModal() {
-    this.activeModal.dismiss('Cross click');
   }
 }

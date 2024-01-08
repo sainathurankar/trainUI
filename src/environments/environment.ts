ffa19ec7c382358c73e99fe78f30f361e0ebca97
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2024-01-08 16:11:09 +0530

    env changes reverted

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  mock: false,
-  apiUrl: 'https://rail-way-api.onrender.com/api',
-  // apiUrl: 'http://localhost:8080/api',
+  mock: true,
+  // apiUrl: 'https://rail-way-api.onrender.com/api',
+  apiUrl: 'http://localhost:8080/api',
   redBusBookingLink: 'https://www.redbus.in/railways/travellerInfo',
   hdfcSmartBuyLink: 'https://offers.reward360.in/v1/train/checkout?searchTrainKey={key}'
 };

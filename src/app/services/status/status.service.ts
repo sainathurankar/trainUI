1398fcb5c5578f621437baa125e551069f2f7916
Urankar, Sainath <Sainath.Urankar@Clarivate.com>
2024-01-27 01:02:25 +0530

    adding message 'Application booting up'

--- src/app/services/status/status.service.ts
+++ src/app/services/status/status.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,13 @@ export class StatusService {
   constructor(private http: HttpClient) { }
 
   getAPIStatus(): Observable<any> {
+
+    if (environment.mock) {
+      return this.http
+        .get('assets/mockjson/status.json')
+        .pipe(delay(2000));
+    }
+
     return this.http.get(
       `${environment.apiUrl}/status`
     );
